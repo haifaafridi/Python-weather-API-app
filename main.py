071e6bcf0484708fb20c28569be61c4af9a32917
Haifa Afridi <haifaafridi8@gmail.com>
2025-09-10 15:43:40 +0500

    Wrote boiler code

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import sys
 import requests
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QLineEdit
 from PyQt5.QtCore import Qt
+
+
+class WeatherApp (QWidget):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
